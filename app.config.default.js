c1d57812a1396f4d8941a64899fb75e632a910b8
lzan13 <lzan13.gg@gmail.com>
2016-12-08 11:59:54 +0800

    updata app.config

--- app.config.default.js
+++ app.config.default.js
@@ -14,7 +14,25 @@ var config = {
     session_secret: 'ml-secret-123',
     // 连接 redis 地址和端口
     redis_host: '127.0.0.1',
-    redis_port: 6379
+    redis_port: 6379,
+    redis_db: 0,
+    redis_password: '',
+
+    // mongodb 本地连接配置
+    mongodb: 'mongodb://127.0.0.1/node_test',
+    // mongodb 远程连接配置
+    // mongodb: 'mongodb://username:password@42.96.192.98/ml-server',
+
+    // 系统错误
+    error_server_db: 1001,
+
+    // 应用级错误
+    error_sign_already_exit: 2001,
+    error_sign_sign_up_failed: 2002,
+    error_sign_user_not_exit: 2003,
+    error_sign_sign_in_failed: 2004,
+    error_sign_invalid_password: 2005,
+    error_sign_username_password_null: 2006
 
 };
 
