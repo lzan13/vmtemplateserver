c7251da9e665dd6ea9a2ec06b0ba15b1f32439c7
lzan13 <lzan13.gg@gmail.com>
2016-12-11 23:39:46 +0800

    update console log

--- api/v1/test.js
+++ api/v1/test.js
@@ -14,7 +14,7 @@ var EventProxy = require('eventproxy');
 var config = require('../../app.config');
 
 var testPost = function (req, res, next) {
-    console.log("收到请求的 body: " + req.body.toString());
+    console.log("收到请求的 body: " + req.body.key1 + req.body.key2);
     /**
      * 将请求结果返回给接口调用者，结果包含状态以及请求的数据
      {
